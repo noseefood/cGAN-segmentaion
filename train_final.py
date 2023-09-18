5230469786c4049726dcfee51bb74a4e15a10363
noseefood <lixuesong00@hotmail.com>
2023-09-18 11:29:36 +0200

    1

--- train_final.py
+++ train_final.py
@@ -270,7 +270,7 @@ parser.add_argument('--image_dir', type=str, default='./data/imgs', help='input 
 parser.add_argument('--mask_dir', type=str, default='./data/masks', help='input mask path')
 parser.add_argument('--lrG', type=float, default='2e-4', help='learning rate')
 parser.add_argument('--lrD', type=float, default='1e-4', help='learning rate')
-parser.add_argument('--optimizer', type=str, default='SGD', help='RMSprop or Adam')
+parser.add_argument('--optimizer', type=str, default='Adam', help='RMSprop or Adam')
 parser.add_argument('--batch_size', type=int, default='8', help='batch_size in training')
 parser.add_argument('--b1', type=float, default=0.5, help='adam: decay of first order momentum of gradient')
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
