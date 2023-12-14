1953320d6775fffed5f6fba84e713c827d6b820e
noseefood <lixuesong00@hotmail.com>
2023-12-14 15:42:05 +0100

    11

---
+++ README.md
@@ -0,0 +1,3 @@
+test_model: saving the checkpoints of final test
+data/test_dataset: saving the test dataset
+eval_baselineWin: script for testing in Win
--- debug.py
+++ debug.py
@@ -1,3 +1,26 @@
-import monai
+from model.Generator import Generator # New structure
 
-metric_ssim = monai.losses.ssim_loss.SSIMLoss(spatial_dims=2)
+import os
+import sys
+from glob import glob
+import cv2
+import time
+import torch
+from PIL import Image
+import numpy as np
+import monai
+from monai.transforms import (
+    Activations,
+    AsDiscrete,
+    Compose,
+    ScaleIntensity,
+    Resize,
+    AddChannel,
+)
+
+from monai.networks.nets import UNet, AttentionUnet
+
+Video_recording = True
+
+
+dir_checkpoint_GAN = 'C:/Research/projects/Learning/Needle_GANseg/analysis/RDB/fine_tune results/1/save_G_Exp/generator_18500.pth'
